24194d908de18a69464dc6a4f06fd9ebc5db973a
bakemonostan <ehizomokhomion@gmail.com>
2022-10-08 07:08:12 -0700

    card updated

--- components/Card/Card.jsx
+++ components/Card/Card.jsx
@@ -2,22 +2,29 @@ import Image from 'next/image';
 import styles from './Card.module.css';
 
 const Card = (props) => {
-  const { imgUrl, size } = props;
-
+  const { imgUrl = '/static/clifford.webp', size = 'medium' } = props;
+  const [imgSrc, setImgSrc] = useState(imgUrl);
   //read up on this concept please
   const classMap = {
     large: styles.lgItem,
     medium: styles.mdItem,
     small: styles.smItem,
   };
+
+  const handleOnError = () => {
+    console.log('hii error');
+    setImgSrc('/static/clifford.webp');
+  };
+
   return (
     <div className={styles.container}>
       Card
       <div className={classMap[size]}>
         <Image
-          src={imgUrl}
+          src={imgSrc}
           alt='image'
           layout='fill'
+          onError={handleOnError}
           className={styles.cardImg}
         />
       </div>
